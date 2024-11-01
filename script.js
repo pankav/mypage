1f70cd5d49ec15d035a6c681cc0ae1f203b07d3e
pankav <55692244+pankav@users.noreply.github.com>
2024-11-01 12:43:00 +0200

    Create script.js
    Added the first Javascript File

---
+++ script.js
@@ -0,0 +1,7 @@
+// JavaScript for adding interactivity
+function showMessage() {
+    const message = document.getElementById("message");
+    message.textContent = "Thank you for visiting my page!";
+    message.style.color = "#9b59b6";
+    message.style.fontSize = "1.2rem";
+}
